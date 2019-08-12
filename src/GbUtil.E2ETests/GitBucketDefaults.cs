8504ad0ae02fe96a06a5cf62148ee734208a6dc7
Ikebe Shodai <SIkebe@users.noreply.github.com>
2019-08-12 14:57:03 +0900

    Specify port

--- src/GbUtil.E2ETests/GitBucketDefaults.cs
+++ src/GbUtil.E2ETests/GitBucketDefaults.cs
@@ -4,7 +4,7 @@ namespace GbUtil.E2ETests
 {
     public static class GitBucketDefaults
     {
-        public static string ConnectionStrings { get; set; } = "Host=localhost;Username=gitbucket;Password=gitbucket;Database=gitbucket";
+        public static string ConnectionStrings { get; set; } = "Host=localhost;Port=3306;Username=gitbucket;Password=gitbucket;Database=gitbucket";
         public static string Owner { get; set; } = "root";
         public static string Password { get; set; } = "root";
         public static string ApiEndpoint { get; set; } = "http://localhost:8080/api/v3/";

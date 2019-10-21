020707283fa4da341258dcf8d9e56c4dec8afde9
Ikebe Shodai <SIkebe@users.noreply.github.com>
2019-10-21 21:04:12 +0900

    Cleanup

--- src/GbUtil.E2ETests/E2ETestBase.cs
+++ src/GbUtil.E2ETests/E2ETestBase.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
 using Octokit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using Xunit;
 
 namespace GbUtil.E2ETests
 {
-    public abstract class E2ETestBase : IClassFixture<GitBucketFixture>
+    public abstract class E2ETestBase : IClassFixture<GitBucketFixture>, IDisposable
     {
+        private bool disposedValue = false;
+
         public E2ETestBase(GitBucketFixture fixture)
         {
             GitBucketFixture = fixture;
+            Credentials = new UsernamePasswordCredentials { Username = GitBucketDefaults.Owner, Password = GitBucketDefaults.Password };
+            WorkingDir = Path.Combine(Path.GetDirectoryName(Assembly.GetAssembly(typeof(E2ETestBase))!.Location)!, Guid.NewGuid().ToString());
         }
 
         public static string GbUtilDll
@@ -25,9 +34,19 @@ namespace GbUtil.E2ETests
             }
         }
 
+        public UsernamePasswordCredentials Credentials { get; }
+        public Octokit.Repository Repository { get; set; } = default!;
+        public string WorkingDir { get; }
+
         public GitBucketFixture GitBucketFixture { get; set; }
 
-        public async Task<Repository> CreateRepository(bool autoInit = false)
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task<Octokit.Repository> CreateRepository(bool autoInit = false)
         {
             var repository = Guid.NewGuid().ToString();
             return await GitBucketFixture.GitBucketClient.Repository.Create(new NewRepository(repository) { AutoInit = autoInit });
@@ -72,6 +91,88 @@ namespace GbUtil.E2ETests
             {
                 RemoveReadonlyAttribute(di);
             }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    var directoryInfo = new DirectoryInfo(WorkingDir);
+                    RemoveReadonlyAttribute(directoryInfo);
+                    directoryInfo.Delete(recursive: true);
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        protected async Task UpdateReadme()
+        {
+            var contents = await GitBucketFixture.GitBucketClient.Repository.Content.GetAllContentsByRef(GitBucketDefaults.Owner, Repository.Name, "develop");
+            var readme = contents.Where(c => c.Name == "README.md").Single();
+            await GitBucketFixture.GitBucketClient.Repository.Content.UpdateFile(
+                GitBucketDefaults.Owner,
+                Repository.Name,
+                "README.md",
+                new UpdateFileRequest("New commit message.", "New file content.", readme.Sha, "develop"));
+        }
+
+        protected void SetMilestone(Issue issue)
+        {
+            if (issue is null)
+            {
+                throw new ArgumentNullException(nameof(issue));
+            }
+
+            GitBucketFixture.Driver.Navigate().GoToUrl(new Uri($"{GitBucketDefaults.BaseUri}{Repository.FullName}/issues/{issue.Number}"));
+            var wait = new WebDriverWait(GitBucketFixture.Driver, new TimeSpan(0, 0, 15));
+            wait.Until(drv => drv.Title == $"{issue.Title} - Issue #{issue.Number} - {Repository.FullName}");
+
+            // GitBucket issue page has multiple elements whose id is "test".
+            // Milestone dropdown is the fourth of them.
+            var tests = GitBucketFixture.Driver.FindElements(By.XPath(@"//*[@id=""test""]"));
+            tests[3].Click();
+            var dropdownMenus = GitBucketFixture.Driver.FindElements(By.XPath("//ul[@class='dropdown-menu pull-right']//li/a"));
+            foreach (var dropdownMenu in dropdownMenus)
+            {
+                var dataTitle = dropdownMenu.GetAttribute("data-title");
+                if (!string.IsNullOrEmpty(dataTitle))
+                {
+                    if (dataTitle.Contains("v1.0.0", StringComparison.OrdinalIgnoreCase))
+                    {
+                        dropdownMenu.Click();
+                        break;
+                    }
+                }
+            }
+        }
+
+        protected void CreateBranch(string name)
+        {
+            // git clone http://localhost:8080/git/root/<repository-name>.git
+            LibGit2Sharp.Repository.Clone($"{GitBucketDefaults.BaseUri}git/{Repository.FullName}.git", WorkingDir, new CloneOptions
+            {
+                CredentialsProvider = (url, user, cred) => Credentials
+            });
+
+            // TODO: Use Web API to create a new branch if implemented in GitBucket
+            using var repo = new LibGit2Sharp.Repository(WorkingDir);
+
+            // git checkout -b <new-branch>
+            repo.CreateBranch("develop");
+            var localBranch = Commands.Checkout(repo, name);
+
+            // git branch -u origin/<new-branch>
+            Remote remote = repo.Network.Remotes["origin"];
+            repo.Branches.Update(localBranch, b => b.Remote = remote.Name, b => b.UpstreamBranch = localBranch.CanonicalName);
+
+            // git push origin <new-branch>
+            repo.Network.Push(repo.Branches[name], new PushOptions
+            {
+                CredentialsProvider = new CredentialsHandler((url, usernameFromUrl, types) => Credentials)
+            });
         }
     }
 }

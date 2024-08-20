d1244e195390b498c7a88c77792c724aafd93a18
SIkebe <SIkebe@users.noreply.github.com>
2024-08-20 16:25:39 +0900

    chore: Update FakeRepository constructor parameters

--- src/GitBucket.Service.Tests/OctokitFakes.cs
+++ src/GitBucket.Service.Tests/OctokitFakes.cs
@@ -10,6 +10,6 @@ public sealed class FakeGitReference : Octokit.GitReference
     public FakeGitReference(string @ref) => Ref = @ref;
 }
 
-public sealed class FakeRepository(User owner, string repository, DateTimeOffset createdAt) : Octokit.Repository(null, null, null, null, null, null, null, null, 0, null, owner, repository, owner?.Login + "/" + repository, false, null, null, null, false, false, 0, 0, null, 0, null, createdAt, createdAt, null, null, null, null, false, false, false, false, false, 0, 0, false, false, false, false, 0, false, RepositoryVisibility.Public, Array.Empty<string>(), false, false, false)
+public sealed class FakeRepository(User owner, string repository, DateTimeOffset createdAt) : Octokit.Repository(null, null, null, null, null, null, null, null, 0, null, owner, repository, owner?.Login + "/" + repository, false, null, null, null, false, false, 0, 0, null, 0, null, createdAt, createdAt, null, null, null, null, false, false, false, false, false, 0, 0, false, false, false, false, 0, false, RepositoryVisibility.Public, [], false, false, false, null)
 {
 }
